222d856b22d86feca354f86bb2c8ac1a8805c62b
keisuke kamiya <luckey.star123@gmail.com>
2013-06-23 09:45:41 +0900

    Main作成

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: keisuke
+ * Date: 13/06/23
+ * Time: 9:45
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+}
