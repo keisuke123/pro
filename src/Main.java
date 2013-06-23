230f421942ed643387b9e211280360b35049569b
keisuke kamiya <luckey.star123@gmail.com>
2013-06-23 10:05:06 +0900

    テンプレ作成
    起動しwindowを表示、サイズ、消したときのオプション（プログラムの終了）を追加

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import javax.swing.*;
+
 /**
  * Created with IntelliJ IDEA.
  * User: keisuke
@@ -5,5 +7,16 @@
  * Time: 9:45
  * To change this template use File | Settings | File Templates.
  */
-public class Main {
+public class Main extends JFrame{
+
+    Main(String name){
+       setTitle(name);
+        setSize(200,200);
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args){
+        new Main("hoge");
+    }
 }
