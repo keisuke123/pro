d0c940ef8089f34535e74ea3322f362ff4485fab
keisuke kamiya <luckey.star123@gmail.com>
2013-06-24 19:38:29 +0900

    認証部分の追加

--- src/LoginActivity.java
+++ src/LoginActivity.java
@@ -1,5 +1,8 @@
 import twitter4j.Twitter;
+import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
+import twitter4j.auth.AccessToken;
+import twitter4j.auth.RequestToken;
 
 import javax.swing.*;
 import java.awt.*;
@@ -19,31 +22,36 @@ import java.net.URISyntaxException;
 public class LoginActivity  extends JFrame{
     JPanel p1 = new JPanel();
     JPanel p2 = new JPanel();
+    JPanel p3 = new JPanel();
     JLabel l1 = new JLabel("認証を行います。");
     JLabel l2 = new JLabel("PIN");
     JButton b1 = new JButton("Open Browser");
+    JButton b2 = new JButton("Enter");
     JTextField tf = new JTextField(15);
 
     Twitter twitter;
+
+    RequestToken rt;
+
+    String url;
 
     LoginActivity(String name){
         setLayout(name);
 
         twitter = new TwitterFactory().getInstance();
-        twitter.setOAuthConsumer(null,null);
-
-    }
-
-    //Layoutについてはこっちで行う。
-    private void setLayout(String name){
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      //  twitter.setOAuthConsumer(null,null);
+        try {
+            rt = twitter.getOAuthRequestToken();
+        } catch (TwitterException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+         url = rt.getAuthorizationURL();
 
         b1.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String str = "http://www.google.com.";
                 try {
-                    URI uri = new URI(str);
+                    URI uri = new URI(url);
                     Desktop dt = Desktop.getDesktop();
                     dt.browse(uri);
                 } catch (URISyntaxException e1) {
@@ -55,6 +63,24 @@ public class LoginActivity  extends JFrame{
             }
         });
 
+        b2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //To change body of implemented methods use File | Settings | File Templates.
+                try {
+                    String text = tf.getText();
+                    AccessToken ac = twitter.getOAuthAccessToken(rt,text);
+                } catch (TwitterException e1) {
+                    e1.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+                }
+            }
+        });
+    }
+
+    //Layoutについてはこっちで行う。
+    private void setLayout(String name){
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
         //メッセージとボタン
         p1.add(l1);
         p1.add(b1);
@@ -63,10 +89,13 @@ public class LoginActivity  extends JFrame{
         p2.add(l2);
         p2.add(tf);
 
+        p3.add(b2);
+
         JDialog dialog = new JDialog();
         dialog.setResizable(false); //フレームのサイズを変更できるかどうか
         dialog.add(p1);
         dialog.add(p2);
+        dialog.add(p3);
         dialog.setTitle(name);
         dialog.setLayout(new FlowLayout());
         dialog.setSize(new Dimension(400,200));
