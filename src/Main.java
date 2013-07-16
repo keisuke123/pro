f0e0d227a3cc587e2549c241c8000cf75140bcf3
keisuke123 <luckey.star123@gmail.com>
2013-07-16 12:13:45 +0900

    DM送信機能を追加

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import twitter4j.Paging;
 import twitter4j.Status;
+import twitter4j.StatusUpdate;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 
@@ -18,12 +19,12 @@ import java.util.List;
 import java.util.Properties;
 
 /**
- * Created with IntelliJ IDEA.
- * User: keisuke
- * Date: 13/06/23
- * Time: 9:45
- * To change this template use File | Settings | File Templates.
- */
+* Created with IntelliJ IDEA.
+* User: keisuke
+* Date: 13/06/23
+* Time: 9:45
+* To change this template use File | Settings | File Templates.
+*/
 public class Main extends JFrame implements ListSelectionListener{
 
     private JPanel panel1;
@@ -40,7 +41,8 @@ public class Main extends JFrame implements ListSelectionListener{
     int old = 0;
     int count=0;
 
-    JList<Object> list;
+    JList list;
+    private Dialog dialog;
     Main(String name){
 
         setTitle(name); //タイトルの設定
@@ -51,7 +53,7 @@ public class Main extends JFrame implements ListSelectionListener{
          * サイズの設定→JPanel#setPreferredSize(Dimention)
          * 透明度の設定→JPanel#setOpaque(boolean)不透明でない場合はtrue
          * 枠線の設定　→JPanel#setBorder(Border) interface border
-         * */
+        * */
 
         JPanel panel1 = new JPanel(); //パネル作成
         final JTextField text = new JTextField();
@@ -68,11 +70,54 @@ public class Main extends JFrame implements ListSelectionListener{
         //値をセット
         text.setMaximumSize(d1);
 
+        JMenuBar menuBar = new JMenuBar();
+        JMenu menu = new JMenu("DM");
+        menuBar.add(menu);
+        JMenuItem item = new JMenuItem("Send DM");
+        final JTextField field = new JTextField();
+        item.addActionListener(new ActionListener() {
+    		@Override
+			public void actionPerformed(ActionEvent e) {
+	            JPanel panel = new JPanel();
+	            final JTextArea text = new JTextArea(3,1);
+	            JButton button = new JButton("送信");
+	            button.addActionListener(new ActionListener() {
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						String str = text.getText();
+						String name = field.getText();
+						Twitter twitter = Utils.getInst();
+						try {
+							twitter.sendDirectMessage(name,str);
+						} catch (TwitterException e1) {
+							// TODO 自動生成された catch ブロック
+							e1.printStackTrace();
+						}
+						dialog.dispose();
+					}
+				});
+	            field.setPreferredSize(new Dimension(Short.MAX_VALUE,50));
+
+	            button.setAlignmentX(0.5f);
+	            text.setLineWrap(true);
+	            panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+	            panel.add(field);
+	            panel.add(text);
+	            panel.add(button);
+				dialog = Utils.makeDialog("DirectMessage",panel);
+				dialog.setVisible(true);
+			}
+		});
+
+
+        menu.add(item);
+        setJMenuBar(menuBar);
+
         panel1.setLayout(new BoxLayout(panel1,BoxLayout.LINE_AXIS));
         panel1.add(text);
         panel1.add(button);
 
-        list = new JList<Object>(timeline.toArray());
+        list = new JList(timeline.toArray());
         list.setCellRenderer(new setTimeline());
         list.addListSelectionListener(this);
 
@@ -102,6 +147,7 @@ public class Main extends JFrame implements ListSelectionListener{
         setVisible(true); //表示
     }
 
+
     @Override
     public void valueChanged(ListSelectionEvent e) {
 
@@ -109,33 +155,28 @@ public class Main extends JFrame implements ListSelectionListener{
 
         if(old!=list.getSelectedIndex()){
             old = list.getSelectedIndex();
-            final JDialog dialog = new JDialog();
             JPanel panel = new JPanel();
             final JTextArea text = new JTextArea(3,1);
             final JButton button = new JButton("送信");
 
             button.setAlignmentX(0.5f);
             text.setLineWrap(true);
+            text.setText("@" + status.getUser().getScreenName()+" ");
             panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
             panel.add(text);
             panel.add(button);
-            dialog.setResizable(true); //フレームのサイズを変更できるかどうか
-            dialog.add(panel);
-            dialog.setLocationRelativeTo(null);
-            dialog.setSize(new Dimension(400, 200));
-            dialog.setTitle("send DirectMessage To @"+status.getUser().getScreenName());
+            final JDialog dialog = Utils.makeDialog("Reply",panel, status);
             dialog.setVisible(true);
-
             button.addActionListener(new ActionListener() {
                 @Override
                 public void actionPerformed(ActionEvent e) {
                     Twitter twitter = Utils.getInst();
                     String message = text.getText();
                     try {
-                        twitter.sendDirectMessage(status.getUser().getScreenName(),message);
-                        dialog.dispose();
+                    	twitter.updateStatus(new StatusUpdate(message).inReplyToStatusId(status.getId()));
+                    	dialog.dispose();
                     } catch (TwitterException e1) {
-                        e1.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+                        e1.printStackTrace(); //To change body of catch statement use File | Settings | File Templates.
                     }
                 }
             });
@@ -154,11 +195,11 @@ public class Main extends JFrame implements ListSelectionListener{
             try {
                 image = new JLabel(new ImageIcon(new URL(status.getUser().getProfileImageURL())));
             } catch (MalformedURLException e) {
-                e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+                e.printStackTrace(); //To change body of catch statement use File | Settings | File Templates.
             }
 
             String tweet = status.getUser().getName();
-            tweet = tweet + "  @" + status.getUser().getScreenName();
+            tweet = tweet + " @" + status.getUser().getScreenName();
             tweet += "\n";
             tweet += status.getText();
             JTextArea name = new JTextArea(tweet);
@@ -193,7 +234,7 @@ public class Main extends JFrame implements ListSelectionListener{
             panel.add(name);
 
 
-            return panel;  //To change body of implemented methods use File | Settings | File Templates.
+            return panel; //To change body of implemented methods use File | Settings | File Templates.
         }
     }
 
@@ -211,7 +252,7 @@ public class Main extends JFrame implements ListSelectionListener{
         try {
             timeline = twitter.getHomeTimeline(new Paging(1,200));
         } catch (TwitterException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+            e.printStackTrace(); //To change body of catch statement use File | Settings | File Templates.
         }
 
         for(Status status : timeline){
