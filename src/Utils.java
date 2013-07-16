4085d7609e28852ac8dc03ebf60bc8c387de450e
keisuke123 <luckey.star123@gmail.com>
2013-07-16 15:59:37 +0900

    つぶやきとStatusを返すメソッド追加

--- src/Utils.java
+++ src/Utils.java
@@ -1,26 +1,20 @@
-import twitter4j.Status;
-import twitter4j.Twitter;
-import twitter4j.TwitterException;
-import twitter4j.TwitterFactory;
+import twitter4j.*;
 import twitter4j.auth.AccessToken;
 
-import java.awt.Dimension;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.List;
 import java.io.*;
-import java.util.List;
-import java.util.Properties;
-
-import javax.swing.JDialog;
-import javax.swing.JPanel;
+import java.util.*;
 
 /**
-* Created with IntelliJ IDEA.
-* User: keisuke
-* Date: 13/07/15
-* Time: 16:28
-* To change this template use File | Settings | File Templates.
-*/
+ * Created with IntelliJ IDEA.
+ * User: muttan
+ * Date: 13/07/16
+ * Time: 13:35
+ * To change this template use File | Settings | File Templates.
+ */
 public class Utils {
-
     static Twitter getInst(){
         Twitter twitter = new TwitterFactory().getInstance();
         twitter.setOAuthAccessToken(new AccessToken(getprop().getProperty("oauth.accessToken"),getprop().getProperty("oauth.accessTokenSecret")));
@@ -41,38 +35,57 @@ public class Utils {
         return prop;
     }
 
-    static JDialog makeDialog (String type,JPanel panel,Status status){
+    static JDialog makeDialog (String type,JPanel panel, Status status){
         JDialog dialog = new JDialog();
-    	dialog.setResizable(true); //フレームのサイズを変更できるかどうか
+        dialog.setResizable(true); //フレームのサイズを変更できるかどうか
         dialog.add(panel);
         dialog.setLocationRelativeTo(null);
         dialog.setSize(new Dimension(400, 200));
         dialog.setTitle("send "+type +" To @"+status.getUser().getScreenName());
-    	return dialog;
+        return dialog;
     }
 
     static JDialog makeDialog (String type,JPanel panel){
-    	JDialog dialog = new JDialog();
-    	dialog.setResizable(true); //フレームのサイズを変更できるかどうか
+        JDialog dialog = new JDialog();
+        dialog.setResizable(true); //フレームのサイズを変更できるかどうか
         dialog.add(panel);
         dialog.setLocationRelativeTo(null);
         dialog.setSize(new Dimension(400, 200));
         dialog.setTitle("send "+type);
-    	return dialog;
+        return dialog;
     }
-/*
-    static List<Status> getTimeline(){
+
+    static ArrayList<String> getTweet(){
+        ArrayList<String> tweet = new ArrayList<String>();
     	Twitter twitter = getInst();
-    	List<Status> list;
-		try {
-			list = twitter.getHomeTimeline();
-		} catch (TwitterException e) {
-			// TODO 自動生成された catch ブロック
-			e.printStackTrace();
-		}
+    	java.util.List<Status> list = null;
+        list = getTimeLine();
+        for(Status status : list){
+            tweet.add(status.getText());
+        }
 
-
-    	return list;
+    	return tweet;
     }
-    */
+
+    static  ResponseList<Status> getTimeLine(){
+        Twitter twitter = getInst();
+        ResponseList<Status> timeline = null;
+        try {
+            timeline = twitter.getHomeTimeline(new Paging(1,200));
+        } catch (TwitterException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+        return timeline;
+    }
+
+    static  ResponseList<Status> getTimeLine(Paging paging){
+        Twitter twitter = getInst();
+        ResponseList<Status> timeline = null;
+        try {
+            timeline = twitter.getHomeTimeline(new Paging(1,200));
+        } catch (TwitterException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+        return timeline;
+    }
 }
